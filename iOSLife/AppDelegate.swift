8a2aa9beb007563810f1f79e183aeeecf7e16d50
Mihir Rathwa <mihir.rathwa@gmail.com>
2017-01-24 17:43:22 -0700

    added Copyrights

--- iOSLife/AppDelegate.swift
+++ iOSLife/AppDelegate.swift
@@ -1,3 +1,17 @@
+// Copyright 2017 Mihir Rathwa,
+//
+// This license provides the instructor Dr. Tim Lindquist and Arizona
+// State University the right to build and evaluate the package for the
+// purpose of determining grade and program assessment.
+//
+// Purpose: This file logs all the App Delegate methods as required
+// in Assignment 2
+//
+// Ser423 Mobile Applications
+// see http://pooh.poly.asu.edu/Mobile
+// @author Mihir Rathwa Mihir.Rathwa@asu.edu
+// Software Engineering, CIDSE, ASU Poly
+// @version January 24, 2017
 //
 //  AppDelegate.swift
 //  iOSLife
--- iOSLife/SecondScreen.swift
+++ iOSLife/SecondScreen.swift
@@ -1,3 +1,17 @@
+// Copyright 2017 Mihir Rathwa,
+//
+// This license provides the instructor Dr. Tim Lindquist and Arizona
+// State University the right to build and evaluate the package for the
+// purpose of determining grade and program assessment.
+//
+// Purpose: This file logs all the methods as required for Second Screen in
+// in Assignment 2
+//
+// Ser423 Mobile Applications
+// see http://pooh.poly.asu.edu/Mobile
+// @author Mihir Rathwa Mihir.Rathwa@asu.edu
+// Software Engineering, CIDSE, ASU Poly
+// @version January 24, 2017
 //
 //  SecondScreen.swift
 //  iOSLife
